059af910e8779d481f67968de52effde15340691
hoches <hoches>
2021-05-01 23:17:15 +0300

    Updated services.py

--- api/services.py
+++ api/services.py
@@ -1,4 +1,6 @@
 from io import StringIO
+
+import numpy as np
 import pandas as pd
 
 from api.google_bucket import download_google_bucket_blob
@@ -77,25 +79,28 @@ def save_material_df(material_df: pd.DataFrame) -> None:
 def get_material_type(material_type_df: pd.DataFrame, material_id: pd.Series, row_name: str) -> str:
     """get kind of material type by material type id"""
     material_type_row = material_type_df.loc[material_type_df.id == material_id, row_name]
-    return material_type_row.values[0]
+    return material_type_row.values[0] if material_type_row.values else np.nan
 
 
 def get_unit_type(unit_of_measure_df, uid_short_name: pd.Series, row_name: str) -> str:
     """get unit of measure name by type"""
     material_type_row = unit_of_measure_df.loc[unit_of_measure_df.short_name == uid_short_name, row_name]
-    return material_type_row.values[0]
+    return material_type_row.values[0] if material_type_row.values else np.nan
 
 
 def get_unit_measure(material_type_df, unit_of_measure_df, material_id: pd.Series, row_name: str) -> str:
     """get short name of unit of measure by material type id"""
     material_unit_value = get_material_type(material_type_df, material_id, 'unit')
-    unit_measure_row = unit_of_measure_df.loc[unit_of_measure_df.name == material_unit_value, row_name]
-    return unit_measure_row.values[0]
+    if material_unit_value is not np.nan:
+        unit_measure_row = unit_of_measure_df.loc[unit_of_measure_df.name == material_unit_value, row_name]
+        return unit_measure_row.values[0] if unit_measure_row.values else np.nan
+    else:
+        return material_unit_value
 
 
-def denormilize_data() -> bool:
+def denormalize_data() -> bool:
     """load data from Google cloud storage,
-        denormilize table Material and add column 'short_name' and 'kind',
+        denormalize table Material and add column 'short_name' and 'kind',
         preparing statistic and counting data for building graphs"""
 
     # Load data from Cloud Storage
@@ -103,7 +108,7 @@ def denormilize_data() -> bool:
     material_type_df = pd.read_csv(StringIO(download_google_bucket_blob('MaterialType.csv')), sep=',')
     unit_of_measure_df = pd.read_csv(StringIO(download_google_bucket_blob('UnitOfMeasure.csv')), sep=',')
 
-    chunk_list = []  # save dataframe of Materials after demoralization
+    chunk_list = []  # save all data frame of Materials after demoralization
     if material_df:
         for material_chunk in material_df:
             # add new fields to Material table
